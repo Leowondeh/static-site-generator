733a83b70bbf0292304472a87dc8fd1eef2a830a
Leowondeh <49104843+Leowondeh@users.noreply.github.com>
2024-12-22 01:35:37 +0200

    Add initial project structure with main script, HTML, CSS, and text node implementation

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+def main():
+    dummy = TextNode("This is a text node", TextType.BOLD, 'https://www.boot.dev')
+
+    print(dummy)
+
+if __name__ == '__main__':
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, target):
+        text_equal = self.text == target.text
+        type_equal = self.text_type == target.text_type
+        url_equal = self.url == target.url
+
+        return text_equal and type_equal and url_equal
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
