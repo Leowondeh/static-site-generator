72bfa33bdfee12103e901d03baee6c14741fac51
Leowondeh <49104843+Leowondeh@users.noreply.github.com>
2025-01-01 01:53:14 +0200

    fix the formatting in tests to be consistent

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,53 +1,21 @@
 import unittest
-
 from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
-        node = HTMLNode(
-            'div',
-            'Hello, world!',
-            None,
-            {'class': 'greeting', 'href': 'https://github.com'},
-        )
-        self.assertEqual(
-            node.properties_to_html(),
-            ' class="greeting" href="https://github.com"',
-        )
+        node = HTMLNode('div', 'Hello, world!', None, {'class': 'greeting', 'href': 'https://github.com'})
+        self.assertEqual(node.properties_to_html(), ' class="greeting" href="https://github.com"')
 
     def test_values(self):
-        node = HTMLNode(
-            'div',
-            'lorem ipsum',
-        )
-        self.assertEqual(
-            node.tag,
-            'div',
-        )
-        self.assertEqual(
-            node.value,
-            'lorem ipsum',
-        )
-        self.assertEqual(
-            node.children,
-            None,
-        )
-        self.assertEqual(
-            node.properties,
-            None,
-        )
+        node = HTMLNode('div', 'lorem ipsum')
+        self.assertEqual(node.tag, 'div')
+        self.assertEqual(node.value, 'lorem ipsum')
+        self.assertEqual(node.children, None)
+        self.assertEqual(node.properties, None)
 
     def test_repr(self):
-        node = HTMLNode(
-            'p',
-            'test text',
-            None,
-            {'class': 'primary'},
-        )
-        self.assertEqual(
-            node.__repr__(),
-            "HTMLNode(p, test text, children: None, {'class': 'primary'})",
-        )
+        node = HTMLNode('p', 'test text', None, {'class': 'primary'})
+        self.assertEqual(node.__repr__(), "HTMLNode(p, test text, children: None, {'class': 'primary'})")
 
     # LeafNode tests
     def test_to_html_no_children(self):
