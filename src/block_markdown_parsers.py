c33a89a860f66061e908cce7f7c6b6ef36cf3c04
Leowondeh <49104843+Leowondeh@users.noreply.github.com>
2025-01-05 17:06:45 +0200

    add block type identification for markdown parsing and corresponding tests

--- src/block_markdown_parsers.py
+++ src/block_markdown_parsers.py
@@ -1,3 +1,10 @@
+block_type_paragraph = 'paragraph'
+block_type_heading = 'heading'
+block_type_code = 'code'
+block_type_quote = 'quote'
+block_type_ulist = 'unordered_list'
+block_type_olist = 'ordered_list'
+
 def markdown_to_blocks(markdown_text: str) -> list:
     if markdown_text in ['', ' '] or markdown_text is None:
         return []
@@ -10,3 +17,29 @@ def markdown_to_blocks(markdown_text: str) -> list:
             continue
         converted_blocks.append(block.strip())
     return converted_blocks
+
+def block_to_block_type(markdown_text: str) -> str:
+    # if the line starts with 1-6 continuous hashtags it's a heading
+    if markdown_text.split()[0] in ['#', '##', '###', '####', '#####', '######']:
+        return block_type_heading
+    
+    # if the line starts and ends with 3x ``` and is longer than 6 (so the conditions don't count eachothers backticks) it's a code block
+    elif markdown_text[:3] == '```' and markdown_text[-3:] == '```' and len(markdown_text) >= 6:
+        return block_type_code
+    
+    # if all lines except empty ones (newlines) start with > it's a quote
+    elif all([line[0] == '>' for line in markdown_text.split('\n') if line != '']):
+        return block_type_quote
+    
+    # if all lines except empty ones (newlines) start with either '* ' or '- ' (with a space) it's an unordered list
+    elif all([line.startswith(('* ', '- ')) for line in markdown_text.split('\n') if line != '']):
+        return block_type_ulist
+    
+    # if all lines except empty ones (newlines) start with a number followed by a . and a space, and the numbers increment by 1 it's an ordered list
+    elif all([line.split('.')[0].isdigit() and line.split('.')[1].startswith(' ') for line in markdown_text.split('\n') if line != '']):
+        # get the first char of every line start and check if it matches an incrementing list
+        line_indexes = [line.split()[0][0] for line in markdown_text.split('\n') if len(line.split()) > 0]
+        if line_indexes == [str(i) for i in range(1, len(line_indexes) + 1)]:
+            return block_type_olist
+    
+    return block_type_paragraph
--- src/test_block_markdown_parsers.py
+++ src/test_block_markdown_parsers.py
@@ -1,5 +1,14 @@
-from block_markdown_parsers import markdown_to_blocks
+from block_markdown_parsers import markdown_to_blocks, block_to_block_type
 import unittest
+
+from block_markdown_parsers import (
+    block_type_paragraph,
+    block_type_heading, 
+    block_type_code, 
+    block_type_quote, 
+    block_type_olist, 
+    block_type_ulist
+)
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_standard(self):
@@ -13,12 +22,12 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
 * This is another list item
 """
         self.assertEqual(
-                        [
-                            '# This is a heading',
-                            'This is a paragraph of text. It has some **bold** and *italic* words inside of it.',
-                            '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
-                        ],
-                        markdown_to_blocks(markdown)
+            [
+                '# This is a heading',
+                'This is a paragraph of text. It has some **bold** and *italic* words inside of it.',
+                '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
+            ],
+            markdown_to_blocks(markdown)
         )
     
     def test_lots_of_newlines(self):
@@ -48,5 +57,66 @@ This is the same paragraph on a new line
         self.assertEqual(markdown_to_blocks(' '), [])
         self.assertEqual(markdown_to_blocks('only one line of text'), ['only one line of text'])
 
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading(self):
+        self.assertEqual(block_type_heading, block_to_block_type('###### a small heading'))
+        self.assertEqual(block_type_heading, block_to_block_type('##### a small heading'))
+        self.assertEqual(block_type_heading, block_to_block_type('#### a medium heading'))
+        self.assertEqual(block_type_heading, block_to_block_type('#### a medium heading'))
+        self.assertEqual(block_type_heading, block_to_block_type('### a big heading'))
+        self.assertEqual(block_type_heading, block_to_block_type('## a big heading'))
+        self.assertEqual(block_type_heading, block_to_block_type('# a huge heading'))
+
+        self.assertEqual(block_type_paragraph, block_to_block_type('#.# an interrupted heading'))
+        self.assertEqual(block_type_paragraph, block_to_block_type('####### a heading with too many #'))
+    
+    def test_code(self):
+        self.assertEqual(block_type_code, block_to_block_type('```a code block```'))
+        self.assertEqual(block_type_code, block_to_block_type('``````'))
+        self.assertEqual(block_type_paragraph, block_to_block_type('````'))
+    
+    def test_quote(self):
+        quote_text = """
+> a quote
+> another quote
+"""
+        self.assertEqual(block_type_quote, block_to_block_type(quote_text))
+    
+    def test_unordered_list(self):
+        unordered_list_asterisk_text = """
+* element one
+* element two
+* element three
+"""
+        unordered_list_dash_text = """
+- element one
+- element two
+- element three
+"""
+        unordered_list_mixed_text = """
+* element one
+- element two
+* element three
+- element four
+"""
+        self.assertEqual(block_type_ulist, block_to_block_type(unordered_list_asterisk_text))
+        self.assertEqual(block_type_ulist, block_to_block_type(unordered_list_dash_text))
+        self.assertEqual(block_type_ulist, block_to_block_type(unordered_list_mixed_text))
+
+    def test_ordered_list(self):
+        ordered_list_correct_order = """
+1. something
+2. something else
+3. something something
+"""
+        ordered_list_wrong_order = """
+1. first element
+3. third element
+2. second element
+4. oopsie daisy i ordered it wrong!
+"""
+        self.assertEqual(block_type_olist, block_to_block_type(ordered_list_correct_order))
+        self.assertEqual(block_type_paragraph, block_to_block_type(ordered_list_wrong_order))
+        
 if __name__ == '__main__':
     unittest.main()
